7e9a894c0ec5fa3ec690856f93c4fe84947bfea4
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-04-30 00:26:35 +0200

    Show the camera preview in an area

--- src/nl/tudelft/followbot/MainActivity.java
+++ src/nl/tudelft/followbot/MainActivity.java
@@ -1,15 +1,67 @@
 package nl.tudelft.followbot;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
+
+	private final String TAG = "bar";
+
+	Camera mCamera;
+	CameraPreview mPreview;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.d("bar", checkCameraHardware(this.getBaseContext()) + "");
+
+		if (checkCameraHardware(this.getBaseContext())) {
+			mCamera = getCameraInstance();
+
+			// Create our Preview view and set it as the content of our
+			// activity.
+			mPreview = new CameraPreview(this, mCamera);
+			FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+			preview.addView(mPreview);
+
+		} else {
+			Log.d(TAG, "Make sure you have a camera");
+		}
+	}
+
+	public void onClick(View view) {
+		Log.d("bar", "clicked");
+	}
+
+	/** Check if this device has a camera */
+	private boolean checkCameraHardware(Context context) {
+		if (context.getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
+			// this device has a camera
+			return true;
+		} else {
+			// no camera on this device
+			return false;
+		}
+	}
+
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
 	}
 
 	@Override
