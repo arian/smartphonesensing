fa399a563706c63ae49de0e6abb6b12bae2e3502
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-05-13 13:28:11 +0200

    Add testcase for FeatureExtractor#avgPower

--- test/nl/tudelft/followbot/data/FeatureExtractorTest.java
+++ test/nl/tudelft/followbot/data/FeatureExtractorTest.java
@@ -1,6 +1,7 @@
 package nl.tudelft.followbot.data;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -23,6 +24,19 @@ public class FeatureExtractorTest {
 		FeatureExtractor extractor = new FeatureExtractor(data);
 		int crossings = extractor.zeroCrossings();
 		assertEquals(3, crossings);
+	}
+
+	@Test
+	public void testPower() {
+		DataStack<Float> data = new DataStack<Float>(4);
+		data.push(1f);
+		data.push(2f);
+		data.push(3f);
+		data.push(4f);
+
+		FeatureExtractor extractor = new FeatureExtractor(data);
+		float pwr = extractor.avgPower();
+		assertTrue(pwr == 7.5f);
 	}
 
 }
