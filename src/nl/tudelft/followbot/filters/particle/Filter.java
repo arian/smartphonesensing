555ea06cf896e70984b07004258464557f8e4a28
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-05-22 11:28:11 +0200

    Plot the particles in 3D

--- src/nl/tudelft/followbot/filters/particle/Filter.java
+++ src/nl/tudelft/followbot/filters/particle/Filter.java
@@ -1,5 +1,8 @@
 package nl.tudelft.followbot.filters.particle;
 
+import javax.swing.JFrame;
+
+import org.math.plot.Plot3DPanel;
 
 public class Filter {
 
@@ -18,23 +21,34 @@ public class Filter {
 
 			particles.add(p);
 		}
-
-		double[][] x = getPositions();
-
 	}
 
 	public double[][] getPositions() {
-		double[][] x = new double[2][particles.size()];
+		double[][] x = new double[3][particles.size()];
 		for (int i = 0; i < particles.size(); i++) {
 			Particle p = particles.get(i);
 			x[0][i] = p.getX();
 			x[1][i] = p.getY();
+			x[2][i] = p.getWeight();
 		}
 		return x;
 	}
 
+	public void plot() {
+		double[][] x = getPositions();
+
+		Plot3DPanel plot = new Plot3DPanel();
+		plot.addScatterPlot("particles", x);
+
+		JFrame frame = new JFrame();
+		frame.setSize(800, 800);
+		frame.setContentPane(plot);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 	static public void main(String[] argv) {
-		new Filter(5000, 10);
+		new Filter(5000, 10).plot();
 	}
 
 }
