f2038457fd0f7c71233a6a59b1f326053382d575
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-05-23 11:10:09 +0200

    Adding Particle Filter normalization and resampling

--- src/nl/tudelft/followbot/filters/particle/Filter.java
+++ src/nl/tudelft/followbot/filters/particle/Filter.java
@@ -26,6 +26,7 @@ public class Filter {
 
 			Particle p = new Particle(r * Math.cos(a), r * Math.sin(a),
 					orientation);
+
 			p.setWeight(1.0 / N);
 			particles.add(p);
 		}
@@ -246,7 +247,20 @@ public class Filter {
 	}
 
 	static public void main(String[] argv) {
-		new Filter(5000, 10).plot();
+		Filter filter = new Filter(2000, 10);
+		for (int j = 0; j < 10; j++) {
+			double x = 4 + Math.random();
+			double y = 4 + Math.random();
+
+			Particles close = filter.getParticles().getParticlesAt(x, y, 100);
+			for (int i = 0; i < close.size(); i++) {
+				close.get(i).setWeight(1 / close.get(i).distanceToSquare(x, y));
+			}
+		}
+		filter.getParticles().normalizeWeights();
+		filter.plot();
+		filter.resample();
+		filter.plot();
 	}
 
 }
